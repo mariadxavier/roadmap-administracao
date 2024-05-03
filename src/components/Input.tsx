6df0298041ac7145bbe7d6f92ed93653260ea1e8
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2024-05-03 19:19:35 -0300

    refactor Input Component Finish

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,19 +6,19 @@ interface IInputProps {
 
 export function Input({ label, placeHolder, icon }: IInputProps) {
   return (
-    <div className="flex flex-col w-4/5 items-center ">
+    <div className="flex flex-col w-4/5 items-center justify-center">
       <label
-        className="self-start ml-4 font-inter font-bold text-sm"
+        className="self-start ml-6 font-inter font-bold text-sm text-[#434343]"
         htmlFor={label}
       >
         {label}
       </label>
-      <label className="w-full relative">
+      <label className="w-[90%] relative">
         <span className="absolute inset-y-0 left-0 flex items-center pl-3">
           {icon}
         </span>
         <input
-          className="w-full bg-gray-200  h-12 rounded-lg pl-10 placeholder:text-[#434343] placeholder:opacity-60"
+          className=" bg-gray-200 w-full  h-12 rounded-lg pl-10 placeholder:text-[#434343] placeholder:opacity-60"
           placeholder={placeHolder}
           name={label}
           type="text"
