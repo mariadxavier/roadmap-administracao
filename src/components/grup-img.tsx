1c887914d1a219d15a5800e9f4d34feaba0e292c
ojpbraga <ojpbraga@gmail.com>
2024-05-03 15:03:35 -0300

    fix: GrupImg

--- src/components/grup-img.tsx
+++ src/components/grup-img.tsx
@@ -4,7 +4,7 @@ import img2 from '../img/young-people-working-at-the-desk.png'
 
 function GrupImg() {
   return (
-    <div className='grid relative  justify-items-center h-3/4 w-5/6 top-8' >
+    <div className="grid relative justify-items-center h-3/4 w-5/6 top-[2vh]">
         <img src={img1} className='absolute h-3/5' alt="" />
         <img src={img2} className='absolute h-full object-cover top-10' alt="" />
     </div>
