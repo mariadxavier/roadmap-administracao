16482c6e11552e530132fd67020d875a6bed3bb4
ojpbraga <ojpbraga@gmail.com>
2024-05-03 15:04:20 -0300

    style: AccessButton

--- src/components/access-button.tsx
+++ src/components/access-button.tsx
@@ -1,3 +1,13 @@
-export function AccessButton() {
-  return <button className="bg-[#0077B6] font-inter">Entrar</button>;
+import { ComponentProps } from "react";
+
+interface PropsAccessButton extends ComponentProps<'button'>{
+
+}
+
+export function AccessButton({...props}: PropsAccessButton) {
+
+
+  return (
+    <button className="bg-adm-dark font-poppins uppercase h-16 w-72 rounded-2xl text-white text-2xl font-bold" {...props}>Entrar</button>
+  )
 }
