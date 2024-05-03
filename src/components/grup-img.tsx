bb33447c4edc151eed87bc0adb0195b366ccebe5
ojpbraga <ojpbraga@gmail.com>
2024-05-03 12:42:04 -0300

    style: new component Grup Img

---
+++ src/components/grup-img.tsx
@@ -0,0 +1,14 @@
+import React, { ComponentProps } from 'react'
+import img1 from '../img/signing-contract.png'
+import img2 from '../img/young-people-working-at-the-desk.png'
+
+function GrupImg() {
+  return (
+    <div className='grid relative  justify-items-center h-3/4 w-5/6 top-8' >
+        <img src={img1} className='absolute h-3/5' alt="" />
+        <img src={img2} className='absolute h-full object-cover top-10' alt="" />
+    </div>
+  )
+}
+
+export default GrupImg
