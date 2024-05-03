2c1e900e8ad3c8050f0fff8dff9f8e18b30c4f40
ojpbraga <ojpbraga@gmail.com>
2024-05-03 16:20:43 -0300

    style: accessButton

--- src/components/access-button.tsx
+++ src/components/access-button.tsx
@@ -8,6 +8,6 @@ export function AccessButton({...props}: PropsAccessButton) {
 
 
   return (
-    <button className="bg-adm-dark font-poppins uppercase h-16 w-72 rounded-2xl text-white text-2xl font-bold" {...props}>Entrar</button>
+    <button className="bg-adm-dark font-poppins uppercase h-16 xsm:h-14 w-72 xsm:w-60 rounded-2xl text-white text-2xl font-bold" {...props}>Entrar</button>
   )
 }
