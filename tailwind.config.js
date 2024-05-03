60b60e88086ac8bbd004148ed10fb940b8fc9219
ojpbraga <ojpbraga@gmail.com>
2024-05-03 01:13:28 -0300

    style: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        adm: {
+          "light": "#e3f2fd",
+          "pale": "#c0dded",
+          "light-sky": "#96d3f4",
+          "sky": "#86d5fe",
+          "medium-sky": "81cdf5",
+          "medium": "#6aaacb",
+          "dark": "#0077b6",
+          "deep": "#006d77",
+      }
+      },
       fontFamily: {
         fascinate: ["Fascinate", "Georgia", "sans-serif"],
         inter: ["Inter", "Arial", "sans-serif"],
