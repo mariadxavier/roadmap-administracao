7579186356fd3ec23d424df92339da3b721bd118
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2024-05-03 17:19:58 -0300

    add && Style: Input Component
    Finalizando o componente de input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,14 +1,29 @@
 interface IInputProps {
   label: string;
   placeHolder?: string;
+  icon?: JSX.Element;
 }
 
-export function Input({ label }: IInputProps) {
- 
+export function Input({ label, placeHolder, icon }: IInputProps) {
   return (
     <div className="flex flex-col w-4/5 items-center ">
-      <label className="self-start ml-4" htmlFor={label}>{label}</label>
-      <input className="w-full bg-gray-200" type="text" />
+      <label
+        className="self-start ml-4 font-inter font-bold text-sm"
+        htmlFor={label}
+      >
+        {label}
+      </label>
+      <label className="w-full relative">
+        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+          {icon}
+        </span>
+        <input
+          className="w-full bg-gray-200  h-12 rounded-lg pl-10 placeholder:text-[#434343] placeholder:opacity-60"
+          placeholder={placeHolder}
+          name={label}
+          type="text"
+        />
+      </label>
     </div>
   );
 }
