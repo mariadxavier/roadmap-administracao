9c2fa3480a1470e7721acc302ff1b6d1e371f400
Madu Xavier <pro.maduxavier@gmail.com>
2024-05-04 20:35:54 -0300

    fix export function

--- src/components/GrupImg.tsx
+++ src/components/GrupImg.tsx
@@ -2,7 +2,7 @@ import React, { ComponentProps } from 'react'
 import img1 from '../assets/signing-contract.png'
 import img2 from '../assets/young-people-working-at-the-desk.png'
 
-function GrupImg() {
+export function GrupImg() {
   return (
     <div className="grid relative justify-items-center h-3/4 w-5/6 lg:w-[70%] lg:h-full top-[2vh]">
         <img src={img1} className='absolute h-3/5 lg:h-4/5' alt="" />
@@ -11,4 +11,3 @@ function GrupImg() {
   )
 }
 
-export default GrupImg
