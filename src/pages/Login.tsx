97332d1a742e57db69bfaa736de4d87d98f13180
Madu Xavier <pro.maduxavier@gmail.com>
2024-05-03 21:35:57 -0300

    fix size duplicado

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,7 +7,7 @@ export function Login() {
         <div className="flex flex-col items-center lg:bg-backgroundLight-adm w-screen h-screen py-4">
             <GoBack />
             <div className="flex items-center flex-col h-full w-full max-w-[560px] lg:bg-white lg:rounded-[20px] lg:pb-6 justify-center gap-8">
-                <div className="size-80 sm:size-80 bg-macaco bg-top bg-auto bg-no-repeat" >
+                <div className="size-80 bg-macaco bg-top bg-auto bg-no-repeat" >
                 </div>
 
                 <h1 className="font-inter font-black text-title-adm text-[2.3rem] leading-8 tracking-[0.63rem]">LOGIN</h1>
