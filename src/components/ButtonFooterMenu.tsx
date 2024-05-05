802965f79383ad4ea054efba2d1ad58fe22951c9
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2024-05-04 22:06:03 -0300

    add Button and FooterMenu Components

---
+++ src/components/ButtonFooterMenu.tsx
@@ -0,0 +1,15 @@
+import { IButtonMenuProps } from "./types";
+
+
+export function ButtonFooterMenu({
+  img,
+  isActive = false,
+}: IButtonMenuProps) {
+  return (
+    <button
+      className={`${isActive ? "bg-[#0A9AE46e]" : ""} size-fit p-1 rounded-lg`}
+    >
+      <img className="size-8" src={img} alt="" />
+    </button>
+  );
+}
---
+++ src/components/FooterMenu.tsx
@@ -0,0 +1,25 @@
+import BookMenuFooter from "../assets/bookMenuFooter.gif";
+import ClosedBooksMenuFooter from "../assets/closedbooksMenuFooter.svg";
+import GearMenuFooter from "../assets/gearMenuFooter.svg";
+import { ButtonFooterMenu } from "./ButtonFooterMenu";
+import { IMenuProps } from "./types";
+
+
+
+export function FooterMenu({ whosActive}: IMenuProps) {
+  return (
+    <footer
+      className="h-12 md:h-16 w-full lg:hidden bg-white border-t border-[#a6a6a699] flex justify-center items-center gap-[5.25rem] xsm:gap-16 "
+    >
+      <ButtonFooterMenu
+        img={ClosedBooksMenuFooter}
+        isActive={whosActive === "closedBooks"}
+      />
+      <ButtonFooterMenu
+        img={BookMenuFooter}
+        isActive={whosActive === "openBook"}
+      />
+      <ButtonFooterMenu img={GearMenuFooter} isActive={whosActive === "gear"} />
+    </footer>
+  );
+}
