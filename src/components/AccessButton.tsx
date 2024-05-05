bba4c7fe595b45864d56ec7b8f408a599561d2ce
ojpbraga <ojpbraga@gmail.com>
2024-05-05 17:19:24 -0300

    fix & feat: props

--- src/components/AccessButton.tsx
+++ src/components/AccessButton.tsx
@@ -1,14 +1,16 @@
 import { ComponentProps } from "react";
+import { twJoin } from "tailwind-merge";
 
 interface PropsAccessButton extends ComponentProps<"button"> {
   text?: string;
   w?: string;
+  className?: string;
 }
 
-export function AccessButton({ text, ...props }: PropsAccessButton) {
+export function AccessButton({text, w, className, ...props }: PropsAccessButton) {
   return (
     <button
-      className={`bg-button-adm font-poppins uppercase h-16  w-11/12  rounded-2xl text-white text-2xl font-bold`}
+      className={twJoin(`bg-button-adm font-poppins uppercase h-16  w-11/12  rounded-2xl text-white text-2xl font-bold`, className)}
       {...props}
     >
       {text}
