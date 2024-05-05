7ca1ca203c1d8bcec1cd5d2112a91ff321af34de
ojpbraga <ojpbraga@gmail.com>
2024-05-05 17:20:30 -0300

    style: style property

--- src/components/GrupImg.tsx
+++ src/components/GrupImg.tsx
@@ -2,10 +2,14 @@ import React, { ComponentProps } from 'react'
 import img1 from '../assets/signing-contract.png'
 import img2 from '../assets/young-people-working-at-the-desk.png'
 
-export function GrupImg() {
+interface PropsGrupImg extends ComponentProps<'div'> {
+
+}
+
+export function GrupImg({...props}: PropsGrupImg) {
   return (
-    <div className="grid relative justify-items-center h-3/4 w-5/6 lg:w-[70%] lg:h-full top-[2vh]">
-        <img src={img1} className='absolute h-3/5 lg:h-4/5' alt="" />
+    <div className="grid relative justify-items-center h-3/4 w-5/6 lg:w-[70%] lg:h-full top-[2vh]" {...props}>
+        <img src={img1} className='absolute object-cover' alt="" />
         <img src={img2} className='absolute h-full object-cover top-10' alt="" />
     </div>
   )
