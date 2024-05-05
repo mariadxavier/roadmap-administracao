e20cbfd5716147b6b7ea897cfb5ca4e10c907cf0
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2024-05-05 20:20:23 -0300

    add Component InputRadioFormQuestion

---
+++ src/components/InputRadioFormQuestion.tsx
@@ -0,0 +1,21 @@
+export function InputRadioFormQuestion({
+  nameQuestion,
+  value,
+  content,
+}: {
+  nameQuestion: string;
+  value: "right" | "wrong";
+  content: string;
+}) {
+  return (
+    <label className="flex items-center ">
+      <input
+        className="appearance-none bg-[#d9d9d9] min-w-8 mix-h-8 size-8 rounded-full transition-colors duration-500 checked:bg-backgroundLight-adm checked:border-[8px] checked:border-[#81bcdb] mr-3"
+        type="radio"
+        value={value}
+        name={nameQuestion}
+      />
+      <p className="font-poppins text-sm ">{content}</p>
+    </label>
+  );
+}
