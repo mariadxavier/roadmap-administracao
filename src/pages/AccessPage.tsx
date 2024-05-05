1509e0441f08c89bfc68fc172d86c9cfd30d9775
ojpbraga <ojpbraga@gmail.com>
2024-05-05 17:21:07 -0300

    feat: AccessPage & CardCourse

---
+++ src/pages/AccessPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import CardCourse from '../components/CardCourse'
+import { GrupImg } from '../components/GrupImg'
+
+function AccessPage() {
+  return (
+    <section className='flex flex-col items-center justify-center w-screen h-screen'>
+      
+      <h1 className="font-poppins font-bold text-titlePages-adm text-4xl md:text-5xl leading-8 top-[5vh] lg:top-8 fixed z-[9999]">Acessos</h1>
+
+      {/* Container Carousel */}
+      <div className='flex flex-nowrap lg:justify-center gap-4 overflow-auto relative top-8 h-[80%] w-screen pl-4 pr-4 md:pl-[15vw] md:pr-[15vw] lg:pl-4 lg:pr-4'>
+        <CardCourse bg='#e3f2fd' bgButton='#0077b6' grupImg={<GrupImg/>} text='TÉCNICO ADMINISTRAÇÃO'/>
+      </div>
+      
+    </section>
+  )
+}
+
+export default AccessPage
