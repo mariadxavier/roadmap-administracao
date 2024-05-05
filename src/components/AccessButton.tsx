7e4d66381e0de6ca9970fe4fbcd3950cc9bcc5b2
Madu Xavier <pro.maduxavier@gmail.com>
2024-05-05 18:51:06 -0300

    feat para reaproveitar button de acesso

--- src/components/AccessButton.tsx
+++ src/components/AccessButton.tsx
@@ -1,17 +1,21 @@
 import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface PropsAccessButton extends ComponentProps<"button"> {
-  text?: string;
-  w?: string;
+    text: string;
+    w?: string;
+    color?: string
 }
 
 export function AccessButton({ text, ...props }: PropsAccessButton) {
-  return (
-    <button
-      className={`bg-button-adm font-poppins uppercase h-16  w-11/12  rounded-2xl text-white text-2xl font-bold`}
-      {...props}
-    >
-      {text}
-    </button>
-  );
+    return (
+        <button
+            className={twMerge(
+              "bg-button-adm font-poppins uppercase h-16  w-11/12  rounded-2xl text-white text-2xl font-bold", props.color ? `${props.color}` : ""
+            )}
+            {...props}
+        >
+            {text}
+        </button>
+    );
 }
