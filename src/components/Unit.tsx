ac9d5eeed84dc61121d99ebdc3d85b198d3db50b
ojpbraga <ojpbraga@gmail.com>
2024-05-09 17:52:09 -0300

    feat: UnitButton

--- src/components/Unit.tsx
+++ src/components/Unit.tsx
@@ -4,14 +4,14 @@ import { twMerge } from "tailwind-merge";
 interface UnitProps {
     numero: string,
     nome: string,
-    children: Array<JSX.Element>,
-    closed? : boolean
-    
+    children?: Array<JSX.Element>,
+    closed? : boolean,
 }
 
-export function Unit({children, numero, nome, closed }: UnitProps) {
+export function Unit({children, numero, nome, closed}: UnitProps) {
+
     return (
-        <div className="w-full sm:w-11/12 lg:w-8/12 bg-backgroundMedium-adm p-6 my-4 rounded-[40px] "> 
+        <div className="w-full sm:w-11/12 lg:w-8/12 bg-backgroundMedium-adm p-6 my-4 rounded-[40px]"> 
             {/* Header - Unidade minimizada: */}
             <div className="flex items-center justify-between w-full  text-white">
                 <div className="font-extrabold font-poppins flex flex-col gap-2">
@@ -25,9 +25,11 @@ export function Unit({children, numero, nome, closed }: UnitProps) {
             {/* Unidade inteira - área da trilha: */}
             <div className={twMerge("flex flex-col items-center ", closed ? "h-0 overflow-hidden" : "")}>
                 <div className="p-10 lg:mt-10 w-full sm:w-7/12 flex flex-col items-center gap-8">
-                    {...children}
+                    {children}
                 </div>
             </div>
         </div>
     );
 }
+
+Unit
--- src/components/UnitButton.tsx
+++ src/components/UnitButton.tsx
@@ -1,19 +1,47 @@
+import { FaStar } from "react-icons/fa";
+import { twMerge } from "tailwind-merge";
+
 interface PropsUnitButton {
-    howMany: number;
+    index: number;
+    arrayLength: number;
 }
 
-export function UnitButton({howMany}: PropsUnitButton) {
+export function UnitButton({index, arrayLength}: PropsUnitButton) {
 
-    const buttonsUnit: JSX.Element[] = []
-    
-    for(let i = 0; i <= howMany; i++) {
-        buttonsUnit.push(
-        <div className='grid place-items-center h-24 w-24 bg-slate-400 rounded-2xl odd:self-start even:self-end'>
-            {i+1}
-        </div>
-    );
+    index = index + 1;
+
+    if(index != arrayLength) {
+        return (
+            <div className={twMerge(`grid place-items-center size-24 rounded-2xl odd:self-start even:self-end shadow-md backdrop-contrast-[${20-index}]`)}>
+                <h1 className="text-5xl font-bold text-gray-700">{index}</h1>
+            </div>
+        )
+    } else {
+        return (
+            <button className="grid place-items-center w-24 h-24 backdrop-contrast-[15] shadow-md rounded-2xl">
+                <FaStar className="size-16 fill-yellow-300" />
+            </button>
+        )
     }
-
-    return buttonsUnit;
     
 }
+
+// interface PropsUnitButton {
+//     howMany: number;
+// }
+
+// export function UnitButton({howMany}: PropsUnitButton) {
+
+//     const buttonsUnit: JSX.Element[] = []
+    
+//     for(let i = 0; i <= howMany; i++) {
+//         buttonsUnit.push(
+//         <div className='grid place-items-center h-24 w-24 bg-slate-400 rounded-2xl odd:self-start even:self-end'>
+//             {i+1}
+//         </div>
+//     );
+//     }
+
+//     return buttonsUnit;
+    
+// }
