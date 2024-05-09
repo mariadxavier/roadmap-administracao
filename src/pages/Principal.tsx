45b2f4c649506667cc29a73d4fe43bce1f2c4097
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2024-05-09 17:33:50 -0300

    fix h1 text Principal Page

--- src/pages/Principal.tsx
+++ src/pages/Principal.tsx
@@ -6,54 +6,54 @@ import { Unit } from "../components/Unit";
 import { UnitButton } from "../components/UnitButton";
 
 export function Principal() {
-    return (
-        <div className="flex">
-            <AsideMenu whosActive="openBook" />
-            <div className="w-screen flex flex-col h-dvh">
-                <main className="overflow-scroll">
-                    <header className="flex flex-col items-center w-full gap-6 py-4">
-                        <Header percent={60} />
-                        <h1 className="w-full sm:w-10/12 self-start mt-16 px-4 sm:px-8 font-poppins font-extrabold text-titlePrincipal-adm text-3xl lg:hidden">
-                            Boas vindas à sua trilha de conhecimento!
-                        </h1>
-                        <div className="size-full lg:hidden">
-                            <AboutUs />
-                        </div>
-                    </header>
-                    <section className="h-full w-full flex flex-col items-center lg:mt-10">
-                        <Unit
-                            children={[<UnitButton howMany={5} />]}
-                            numero="1"
-                            nome="Nome da Unidade"
-                        />
-                        <Unit
-                            children={[<UnitButton howMany={5} />]}
-                            closed
-                            numero="1"
-                            nome="Nome da Unidade"
-                        />
-                        <Unit
-                            children={[<UnitButton howMany={5} />]}
-                            closed
-                            numero="1"
-                            nome="Nome da Unidade"
-                        />
-                        <Unit
-                            children={[<UnitButton howMany={5} />]}
-                            closed
-                            numero="1"
-                            nome="Nome da Unidade"
-                        />
-                    </section>
-                </main>
-                <FooterMenu whosActive="openBook" />
+  return (
+    <div className="flex">
+      <AsideMenu whosActive="openBook" />
+      <div className="w-screen flex flex-col h-dvh">
+        <main className="overflow-scroll">
+          <header className="flex flex-col items-center w-full gap-6 py-4">
+            <Header percent={60} />
+            <h1 className="w-full sm:w-10/12 self-start mt-16 px-4 sm:px-8 font-poppins font-extrabold text-titlePrincipal-adm text-3xl lg:hidden xsm:text-2xl">
+              Boas vindas à sua trilha de conhecimento!
+            </h1>
+            <div className="size-full lg:hidden">
+              <AboutUs />
             </div>
-        </div>
-    );
+          </header>
+          <section className="h-full w-full flex flex-col items-center lg:mt-10">
+            <Unit
+              children={[<UnitButton howMany={5} />]}
+              numero="1"
+              nome="Nome da Unidade"
+            />
+            <Unit
+              children={[<UnitButton howMany={5} />]}
+              closed
+              numero="1"
+              nome="Nome da Unidade"
+            />
+            <Unit
+              children={[<UnitButton howMany={5} />]}
+              closed
+              numero="1"
+              nome="Nome da Unidade"
+            />
+            <Unit
+              children={[<UnitButton howMany={5} />]}
+              closed
+              numero="1"
+              nome="Nome da Unidade"
+            />
+          </section>
+        </main>
+        <FooterMenu whosActive="openBook" />
+      </div>
+    </div>
+  );
 }
 
 {
-    /* 
+  /* 
         <andre>
         unidades.map((unidade, tema)
           return(
