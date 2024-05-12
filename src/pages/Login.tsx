f6c984d67e0a1b3a6fb0e2c79ceae7de1e850a5d
Madu Xavier <pro.maduxavier@gmail.com>
2024-05-12 20:14:02 -0300

    feat link to signin from login
    + fix tamanho svg

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,14 +2,15 @@ import { GoBack } from "../components/GoBack";
 import { AccessButton } from "../components/AccessButton";
 import { Input } from "../components/Input";
 import { FaEnvelope, FaLock } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export function Login() {
     return (
-        <div className="flex flex-col items-center lg:bg-backgroundLight-adm w-screen h-dvh py-4">
+        <div className="flex flex-col items-center lg:bg-backgroundLight-adm w-dwh h-dvh py-4">
             <GoBack />
-            <div className="flex items-center flex-col h-full w-full max-w-[560px] lg:bg-white lg:rounded-[20px] lg:pb-6 justify-center gap-8">
+            <div className="flex items-center flex-col h-full w-full max-w-[560px] lg:bg-white lg:rounded-[20px] lg:pb-6 justify-center gap-4">
 
-                <img className="size-80 bg-top bg-auto bg-no-repeat" src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/See-No-Evil%20Monkey.png" alt="See-No-Evil Monkey"/>
+                <img className="xsm:hidden size-60 bg-top bg-auto bg-no-repeat" src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/See-No-Evil%20Monkey.png" alt="See-No-Evil Monkey"/>
 
                 <h1 className="font-poppins font-black text-title-adm text-[2.3rem] leading-8 tracking-[0.63rem]">LOGIN</h1>
 
@@ -19,6 +20,9 @@ export function Login() {
 
                     <Input label="Senha" placeHolder="Senha de acesso" icon={<FaLock size={22} className="text-[#808080]" />}></Input>
                     <AccessButton text="ENTRAR"/>
+                <Link to={"/signin"} className="font-poppins text-sm text-nowrap">
+                    Não tem conta ainda? <span className="font-bold text-titlePrincipal-adm">Cadastre-se!</span>
+                </Link>
                 </form>
             </div>
         </div>
