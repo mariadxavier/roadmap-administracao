5b4dee50cb4de1bc1fada6e6b505e231def7314f
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2024-05-17 17:06:00 -0300

    solving conflicts

--- src/components/Unit.tsx
+++ src/components/Unit.tsx
@@ -4,59 +4,59 @@ import { useRef, useState } from "react";
 import gsap from "gsap";
 
 interface UnitProps {
-    numero: string;
-    nome: string;
-    children?: Array<JSX.Element>;
-    color: string;
+  numero: string;
+  nome: string;
+  children?: Array<JSX.Element>;
+  color: string;
 }
 
 export function Unit({ children, numero, nome, color }: UnitProps) {
-    const unitMaximizada = useRef(null);
-    const [direction, setDirection] = useState("0");
-    const [h, setH] = useState("100%");
+  const unitMaximizada = useRef(null);
+  const [direction, setDirection] = useState("0");
+  const [h, setH] = useState("100%");
 
-    function handleClickUnity() {
-        h === "0" ? setH("100%") : setH("0");
-        h === "0" ? setDirection("0") : setDirection("90");
-        gsap.to(unitMaximizada.current, {
-            duration: 1.8,
-            ease: "bounce.out",
-            height: h,
-        });
-    }
+  function handleClickUnity() {
+    h === "0" ? setH("100%") : setH("0");
+    h === "0" ? setDirection("0") : setDirection("90");
+    gsap.to(unitMaximizada.current, {
+      duration: 1.8,
+      ease: "bounce.out",
+      height: h,
+    });
+  }
 
-    return (
-        <div
-            className="w-11/12 sm:w-11/12 lg:w-8/12 bg-backgroundMedium-adm p-6 mt-4 rounded-[40px]"
-            style={{ background: `${color}` }}
-        >
-            {/* Header - Unidade minimizada: */}
-            <div
-                onClick={handleClickUnity}
-                className="flex items-center justify-between w-full  text-white cursor-pointer"
-            >
-                <div className="font-extrabold font-poppins flex flex-col gap-2">
-                    <h1 className=" text-2xl">Unidade {numero}</h1>
-                    {/* alimentar numero por quantidade e orgem de elementos unit na pagina */}
-                    <h2 className="text-xl">{nome}</h2>
-                </div>
-                <FaPlay
-                    className={twMerge(
-                        "size-8 m-1 ",
-                        `rotate-${direction} transition duration-[1.6s]`
-                    )}
-                />
-            </div>
-
-            {/* Unidade inteira - área da trilha: */}
-            <div
-                ref={unitMaximizada}
-                className="flex flex-col items-center overflow-hidden h-0"
-            >
-                <div className="p-10 lg:mt-10 w-full sm:w-7/12 flex flex-col items-center gap-8">
-                    {children}
-                </div>
-            </div>
+  return (
+    <div
+      className="w-11/12 sm:w-11/12 lg:w-8/12 bg-backgroundMedium-adm p-6 mt-4 rounded-[40px]"
+      style={{ background: `${color}` }}
+    >
+      {/* Header - Unidade minimizada: */}
+      <div
+        onClick={handleClickUnity}
+        className="flex items-center justify-between w-full  text-white cursor-pointer"
+      >
+        <div className="font-extrabold font-poppins flex flex-col gap-2">
+          <h1 className=" text-2xl">Unidade {numero}</h1>
+          {/* alimentar numero por quantidade e orgem de elementos unit na pagina */}
+          <h2 className="text-xl">{nome}</h2>
         </div>
-    );
+        <FaPlay
+          className={twMerge(
+            "size-8 m-1 ",
+            `rotate-${direction} transition duration-[1.6s]`
+          )}
+        />
+      </div>
+
+      {/* Unidade inteira - área da trilha: */}
+      <div
+        ref={unitMaximizada}
+        className="flex flex-col items-center overflow-hidden h-0"
+      >
+        <div className="p-10 lg:mt-10 w-full sm:w-7/12 flex flex-col items-center gap-8">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Principal.tsx
+++ src/pages/Principal.tsx
@@ -1,3 +1,4 @@
+// import { AboutUs } from "../components/AboutUs";
 import { AboutUs } from "../components/AboutUs";
 import { AsideMenu } from "../components/AsideMenu";
 import { FooterMenu } from "../components/FooterMenu";
@@ -7,8 +8,18 @@ import { UnitButton } from "../components/UnitButton";
 
 export function Principal() {
   // Teste de retorno do backend
-  const temas: {nome: string}[]= [
-    {nome: "Matéria 1",},{nome: "Matéria 2"},{nome: "Matéria 3"},{nome: "Matéria 1",},{nome: "Matéria 2"},{nome: "Matéria 3"},{nome: "Matéria 2"},{nome: "Matéria 3"},{nome: "Matéria 2"},{nome: "Matéria 3"}]
+  const temas: { nome: string }[] = [
+    { nome: "Matéria 1" },
+    { nome: "Matéria 2" },
+    { nome: "Matéria 3" },
+    { nome: "Matéria 1" },
+    { nome: "Matéria 2" },
+    { nome: "Matéria 3" },
+    { nome: "Matéria 2" },
+    { nome: "Matéria 3" },
+    { nome: "Matéria 2" },
+    { nome: "Matéria 3" },
+  ];
 
     return (
       <div className="flex">
@@ -29,26 +40,35 @@ export function Principal() {
                             {temas.map((item, index) => {
                               // este log existe apenas pra retirar o warning q impede o deploy no vercel por falta de uso de "item". Necessário outra solução.
 
-                              console.log(item.nome)
-                              return(
-                            
-                            <UnitButton key={index} index={index} arrayLength={temas.length} />) })}
-                          </Unit>
-                          <Unit color="#86d5fe" numero="1" nome="Marketing">
-                            {temas.map((item, index) => {
-                              // este log existe apenas pra retirar o warning q impede o deploy no vercel por falta de uso de "item". Necessário outra solução.
-                              console.log(item.nome)
-                              return(
-                            
-                            <UnitButton key={index} index={index} arrayLength={temas.length} />) })}
-                          </Unit>
-                       
-                    </section>
-                </main>
-                <FooterMenu whosActive="openBook" />
-            </div>
-        </div>
-    );
+                console.log(item.nome);
+                return (
+                  <UnitButton
+                    key={index}
+                    index={index}
+                    arrayLength={temas.length}
+                  />
+                );
+              })}
+            </Unit>
+            <Unit color="#86d5fe" numero="1" nome="Marketing">
+              {temas.map((item, index) => {
+                // este log existe apenas pra retirar o warning q impede o deploy no vercel por falta de uso de "item". Necessário outra solução.
+                console.log(item.nome);
+                return (
+                  <UnitButton
+                    key={index}
+                    index={index}
+                    arrayLength={temas.length}
+                  />
+                );
+              })}
+            </Unit>
+          </section>
+        </main>
+        <FooterMenu whosActive="openBook" />
+      </div>
+    </div>
+  );
 }
 
 {
