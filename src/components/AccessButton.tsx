2de2eab877a5f6091f48f0338da19fc89a95dd88
mariadxavier <pro.maduxavier@gmail.com>
2024-05-20 16:41:11 -0300

    add prop disabled em style de AccessButton

--- src/components/AccessButton.tsx
+++ src/components/AccessButton.tsx
@@ -2,15 +2,16 @@ import { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface PropsAccessButton extends ComponentProps<"button"> {
-    text: string;
-    color?: string
+    text: string,
+    color?: string,
+    disabled? : boolean,
 }
 
-export function AccessButton({ text, ...props }: PropsAccessButton) {
+export function AccessButton({ text, disabled, ...props }: PropsAccessButton) {
     return (
         <button
             className={twMerge(
-              "bg-button-adm font-poppins uppercase h-16  w-11/12  rounded-2xl text-white text-2xl font-bold", props.color ? `${props.color}` : ""
+              "bg-button-adm font-poppins uppercase h-16  w-11/12  rounded-2xl text-white text-2xl font-bold", props.color ? `${props.color}` : "", disabled ? "cursor-not-allowed " : ""
             )}
             {...props}
         >
