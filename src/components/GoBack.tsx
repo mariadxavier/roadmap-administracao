1f25ac6437d0226c34187701047c706fc25ce46b
mariadxavier <pro.maduxavier@gmail.com>
2024-05-20 16:23:47 -0300

    add navegação em goBack

--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -1,8 +1,10 @@
 import { FaArrowLeft } from "react-icons/fa";
+import { useNavigate} from "react-router-dom";
 
 export function GoBack(){
+    const navigate = useNavigate();
     return(
-        <button className="bg-button-adm w-12 h-12 p-0.5 flex items-center justify-center rounded-full fixed top-4 left-4">
+        <button onClick={() => navigate(-1)} className="bg-button-adm w-12 h-12 p-0.5 flex items-center justify-center rounded-full fixed top-4 left-4">
             <FaArrowLeft className="text-white size-6" />
         </button>
     )
