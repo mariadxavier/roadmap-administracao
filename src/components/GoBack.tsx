48bb3cc8d15f155a150eb64d7eb63c00bbd38234
Madu Xavier <pro.maduxavier@gmail.com>
2024-05-27 19:01:54 -0300

    fix z-index goback button

--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -4,7 +4,7 @@ import { useNavigate} from "react-router-dom";
 export function GoBack(){
     const navigate = useNavigate();
     return(
-        <button onClick={() => navigate(-1)} className="bg-button-adm w-12 h-12 p-0.5 flex items-center justify-center rounded-full fixed top-4 left-4">
+        <button onClick={() => navigate(-1)} className="bg-button-adm w-12 h-12 p-0.5 flex items-center justify-center rounded-full fixed top-4 left-4 z-50">
             <FaArrowLeft className="text-white size-6" />
         </button>
     )
