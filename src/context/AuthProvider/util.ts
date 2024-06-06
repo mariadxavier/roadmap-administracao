e17e415b6580c9df820966615575c3f99b3df4f4
Madu Xavier <pro.maduxavier@gmail.com>
2024-06-06 18:44:36 -0300

    add function SignUpUser
    para uso do auth, refatoração

--- src/context/AuthProvider/util.ts
+++ src/context/AuthProvider/util.ts
@@ -1,18 +1,18 @@
 import { IUser } from "./types";
 
 export function setUserLocalStorage(user: IUser | null) {
-    localStorage.setItem('u', JSON.stringify(user));
+    localStorage.setItem("u", JSON.stringify(user));
 }
 
-export function getUserLocalStorage (){
-    const json = localStorage.getItem('u');
+export function getUserLocalStorage() {
+    const json = localStorage.getItem("u");
 
-    if(!json){
-        return null
+    if (!json) {
+        return null;
     }
 
     const user = JSON.parse(json);
-    
+
     return user ?? null;
 }
 
@@ -24,6 +24,25 @@ export async function LoginRequest(email: string, password: string) {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({ email, password }),
+        });
+        return request.json();
+    } catch (error) {
+        return null;
+    }
+}
+
+export async function SignUpUser(
+    nick: string,
+    email: string,
+    password: string
+) {
+    try {
+        const request = await fetch("http://localhost:3000/user", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({nick: nick, email: email, password: password}),
         });
         return request.json();
     } catch (error) {
