22b8b2b81ba89294e0b7887f55237a121aef89ad
João Pedro O. Braga <127335857+ojpbraga@users.noreply.github.com>
2024-06-10 16:26:05 -0300

    test: navegation bar

--- src/components/ButtonFooterMenu.tsx
+++ src/components/ButtonFooterMenu.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { IButtonMenuProps } from "./types";
 
 
@@ -5,9 +6,30 @@ export function ButtonFooterMenu({
   img,
   isActive = false,
 }: IButtonMenuProps) {
+
+  const navigate = useNavigate();
+
+  function handleClick(event: Event) {
+    // console.log(event.target.src.replace("http://localhost:5173/src/assets/", "").slice(0, -4));
+
+    const selectedItem = event.target.src.replace("http://localhost:5173/src/assets/", "").slice(0, -4);
+    console.log(selectedItem)
+
+    if(selectedItem === "bookMenuFooter") {
+      navigate("/trilha");
+    }
+    if(selectedItem === "gearMenuFooter") {
+      navigate("/configuracoes")
+    }
+    if(selectedItem === "closedbooksMenuFooter") {
+      navigate("/modulos")
+    }
+    
+  }
+
   return (
     <button
-      className={`${isActive ? "bg-[#0A9AE46e]" : ""} size-fit p-1 rounded-lg`}
+      className={`${isActive ? "bg-[#0A9AE46e]" : ""} size-fit p-1 rounded-lg`} onClick={handleClick}
     >
       <img className="size-8" src={img} alt="" />
     </button>
